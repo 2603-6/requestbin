283e2d3d9868615cf195c36e9b8d72d35215cf67
kess <kesslermentele@gmail.com>
2026-03-06 15:59:58 -0800

    Fixed Bin not providing key to dummy Request components.

--- client/src/components/Bin.tsx
+++ client/src/components/Bin.tsx
@@ -10,7 +10,7 @@ const Bin: FC<IBinProps> = ({ binId, capacity, requests }) => {
       <p>Requests are collected at SOME_URL/{binId}</p>
       {capacity ? <p>Capacity:{capacity}</p> : null }
       {requests.map((request) => (
-        <p>RequestConnectionGoesHere {request.type}</p>
+        <p key={request.requestId}>RequestConnectionGoesHere {request.type}</p>
       ))}
     </div>
   );
