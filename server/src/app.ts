a9e7c4b5cce8758c4360cc53f0586ba6ae0bb2b6
JOSH CRANE <onji@JOSHs-MacBook-Pro.local>
2026-03-06 21:36:27 -0800

    added a docker-compose file to setup databases

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
-import express from 'express';
-
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
+dotenv.config();
+console.log(process.env.DB_USER)
 
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World!")
+})
 
-app
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
 const greeting: string = 'Hello';
 const numbers: number[] = [1, 2, 3]
