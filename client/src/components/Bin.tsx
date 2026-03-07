176e147060de0d2b99fecd9e5deba485318de155
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2026-03-07 12:00:05 -0800

    modified Bin component

--- client/src/components/Bin.tsx
+++ client/src/components/Bin.tsx
@@ -1,16 +1,36 @@
+import { useState, useEffect } from 'react';
+import binService from '../services/binService';
 import type { FC } from 'react';
 import type { IBinProps } from '../types';
 
-const Bin: FC<IBinProps> = ({ binId, capacity, requests }) => {
+const Bin: FC<IBinProps> = ({ binId, capacity }) => {
+  const [requests, setRequests] = useState([]);
 
+  
+  useEffect(() => {
+    const fetchRequests = async () => {
+      const data = await binService.fetchRequests();
+      console.log(data);
+      setRequests(data);
+    };
 
+    fetchRequests();
+  }, []);
+
+  // add formatting to date to convert from epoch time to formatted date
   return (
     <div>
       <h1>Bin:{binId}</h1>
       <p>Requests are collected at SOME_URL/{binId}</p>
       {capacity ? <p>Capacity:{capacity}</p> : null }
       {requests.map((request) => (
-        <p key={request.requestId}>RequestConnectionGoesHere {request.type}</p>
+        <div key={request.id}>
+          <p>{request.method}</p>
+          <p>{request.path} and query params here</p>
+          <p>{request.date}</p>
+          <p>Headers - add click event to expand</p>
+          <p>Body - add click event to expand, don't show if empty</p>
+        </div>
       ))}
     </div>
   );
