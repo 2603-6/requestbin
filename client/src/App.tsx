d8707e1ad37e08d51eb6f0787e4ebe0f4ab70448
mahmoudjiha <107807116+mahmoudjiha@users.noreply.github.com>
2026-03-07 11:28:21 -0800

    remove example request from App.tsx, edited IBinProps type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css';
 import Bin from './components/Bin.tsx';
-import type { IRequestProps } from './types';
 
 function App() {
-  const exampleRequest: IRequestProps = {
-    requestId: 5,
-    requestHeaders: 'headers',
-    timestamp: 'timestamp',
-    route: 'route',
-    type: 'GET',
-  };
-
   return (
     <div>
-      <Bin binId={6} requests={[exampleRequest]}/>
+      <Bin binId={6}/>
     </div>
   );
 }
