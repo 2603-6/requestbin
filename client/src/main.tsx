6804cfc26cfac708f0382d28feb0a8bef11736dc
kess <kesslermentele@gmail.com>
2026-03-08 09:47:32 -0700

    Introduce BinContext for dependency injection; refactor Bin and App to use context-based bin service

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { BinContext } from './contexts/binContext.tsx';
+import fakeBinService from './services/fakeBinService.ts';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BinContext value={fakeBinService}>
+      <App />
+    </BinContext>
   </StrictMode>,
 );
