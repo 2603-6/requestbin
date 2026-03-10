6779f67d2aa3d3e078902d6166296e41b7b6496e
Josh C <psycholomo@gmail.com>
2026-03-09 19:43:14 -0700

    re-arranged app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,44 +1,18 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { postgresGetAllBins } from './utils/database_queries';
-import { mongoFindAllRequestById } from './utils/database_queries';
+import { mongoDBConnect } from "./utils/database_connections"
+import { mongoFindAllRequestById, 
+        insertIntoMongo,
+        postgresGetAllBins,
+        postgresGetAllRequests,
+        postgresCreateBin,
+        postgresInsertRequest, } from './utils/database_queries';
 
-const mockGetRequestsByBinName = async (binName: String) => {
-    return {
-        'bin_name': binName,
-        "requests": [
-            {
-                "id": 1,
-                "bin_name": binName,
-                "time_of_day": "10:15:30",
-                "date_stamp": "03:07:2026",
-                "http_method": "POST",
-                "body": "Hello, world!",
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "path": "/test"
-            },
-            {
-                "id": 2,
-                "bin_name": binName,
-                "time_of_day": "11:45:00",
-                "date_stamp": "03:07:2026",
-                "http_method": "POST",
-                "body": "Goodbye, world!",
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "path": "/test"
-            }
-        ]
-    }
-}
 
 const app = express();
 app.use(express.json());
 dotenv.config();
-console.log(process.env.DB_USER)
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World!")
@@ -63,12 +37,7 @@ app.get('/api/bins', async (req: Request, res: Response) => {
 app.get('/api/bins/:binName/requests', async (req: Request, res: Response) => {
     try {
         const binName = req.params.binName as string;
-        const finalResponse = await mockGetRequestsByBinName(binName);
-
-        // kass
-        //const pgMetadata = await postgresGetAllRequests(binName);
-
-
+        const finalResponse = await postgresGetAllRequests(binName);
 
         console.log('heres the json');
         res.status(200).json(finalResponse);
@@ -82,7 +51,7 @@ app.get('/api/bins/:binName/requests', async (req: Request, res: Response) => {
 })
 
 // webhook endpoint
-app.all('/bins/:binName', (req: Request, res: Response) => {
+app.all('/bins/:binName', async (req: Request, res: Response) => {
     const time_received = new Date()
     const timeStamp = time_received.toLocaleString('en-US');
 
@@ -103,11 +72,23 @@ app.all('/bins/:binName', (req: Request, res: Response) => {
 
     console.log("pgData is", pgData);
     console.log("mongoData is", mongoData);
+
+    const mongoResult = await insertIntoMongo(mongoData)
+    const mongodbID = mongoResult.insertedId.toString();
+    await postgresInsertRequest()
     res.status(200).send(`method is ${req.method}`);
     // db insertions precede response above
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
+
+const startServer = async () => {
+    const PORT = 3000;
+    await mongoDBConnect();
+    app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+
+}
+
+startServer();
