6f116b4bc24beb58b0ed8e6cf7ff9ad63d934d0e
kess <kesslermentele@gmail.com>
2026-03-13 09:54:36 -0700

    Switch to binService in BinServiceContext for production use

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BinServiceContext } from './contexts/binServiceContext.ts';
-import binServiceLocal from './services/binServiceLocal.ts';
+import binService from './services/binService.ts';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BinServiceContext value={binServiceLocal}>
+    <BinServiceContext value={binService}>
       <App />
     </BinServiceContext>
   </StrictMode>,
