01a09a6f77e7d06ae51f0de6dfe81d14d2bfc8a3
kess <kesslermentele@gmail.com>
2026-03-13 08:36:09 -0700

    Switch to local binService for development and simplify CORS configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BinServiceContext } from './contexts/binServiceContext.ts';
-import binService from './services/binService.ts';
+import binServiceLocal from './services/binServiceLocal.ts';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BinServiceContext value={binService}>
+    <BinServiceContext value={binServiceLocal}>
       <App />
     </BinServiceContext>
   </StrictMode>,
---
+++ client/src/services/binServiceLocal.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import type { BinProvider, RawBin, RawRequest } from '../types';
+import { parseRawBin, parseRawRequest } from '../utils.ts';
+
+const BASE_URL = 'http://localhost:3000';
+const BINS_URL = BASE_URL + '/api/bins';
+const WS_URL = 'ws://localhost:3000/ws';
+
+const fetchRequests = async (binName: string) => {
+  console.log(`fetching requests for bin ${binName}`);
+  const response = await axios.get<RawRequest[]>(BINS_URL + `/${binName}/requests`);
+  return response.data.map(parseRawRequest);
+};
+
+
+const createBin = async (binName?: string) => {
+  console.log(`creating bin ${binName}`);
+  const response = await axios.post<{ id: RawBin }>(BINS_URL, { bin_name: binName });
+  return parseRawBin(response.data.id);
+};
+
+const fetchBins = async () => {
+  console.log('fetching bins');
+  const response = await axios.get<{ bin_names: RawBin[] }>(BINS_URL);
+  return response.data.bin_names.map(parseRawBin);
+};
+
+
+const deleteBin = async (binName: string) => {
+  console.log(`deleting bin ${binName}`);
+  await axios.delete(BINS_URL + `/${binName}`);
+};
+
+const clearBin = async (binName: string) => {
+  console.log(`deleting all requests in ${binName}`);
+  await axios.delete(BINS_URL + `/${binName}/requests`);
+};
+
+const binService: BinProvider = {
+  fetchRequests,
+  fetchBins,
+  createBin,
+  deleteBin,
+  clearBin,
+  wsUrl: WS_URL,
+  url: BASE_URL,
+};
+
+export default binService;
